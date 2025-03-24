2cdecbc0ad588f908c7a690731879e0531b7e721
SornchaiTheDev <imdev@SornchaiTheDev.local>
2025-03-24 18:47:11 +0700

    chore: uncomment useState

--- src/app/(authed)/teacher/[courseID]/chapters/[chapterID]/settings/page.tsx
+++ src/app/(authed)/teacher/[courseID]/chapters/[chapterID]/settings/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import MaterialsDetail from "@/components/MaterialsDetail";
 import { Trash } from "lucide-react";
-// import { useState } from "react";
+import { useState } from "react";
 
 // interface Props {
 //   course: string;
