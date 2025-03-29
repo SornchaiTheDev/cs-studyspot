1e3c9642569957c6913f8d683e6a0d9d6d6e33aa
Nut <vimudakorn.k@gmail.com>
2025-03-30 01:14:40 +0700

    update: update course

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Afacad } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 
 const afacad = Afacad({
   variable: "--font-afacad",
